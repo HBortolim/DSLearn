7d7e2dbca22921c892230ab18fecfb06296761c2
Henrique Bortolim <hbortolim@gmail.com>
2022-07-15 20:30:09 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,3 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://cdn.iconscout.com/icon/free/png-256/education-cap-2130827-1794972.png','https://upload.wikimedia.org/wikipedia/commons/e/e8/Education%2C_Studying%2C_University%2C_Alumni_-_icon.png');
