c375838599d1a77a165b56076faaa4c13f0b238b
Henrique Bortolim <hbortolim@gmail.com>
2022-07-17 13:46:39 -0300

    Resource

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -18,3 +18,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','htt
 
 INSERT INTO tb_offer(edition,start_Moment,end_Moment,course_id) VALUES('1.0',TIMESTAMP WITH TIME ZONE '2020-09-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-09-20T03:00:00Z',1);
 INSERT INTO tb_offer(edition,start_Moment,end_Moment,course_id) VALUES('2.0',TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z',1);
+
+INSERT INTO tb_resource(title,description,position,img_Uri,type,offer_id) VALUES('Trilha HTML','Trilha principal do curso',1,'https://cdn.iconscout.com/icon/free/png-256/education-cap-2130827-1794972.png', 1,1);
+INSERT INTO tb_resource(title,description,position,img_Uri,type,offer_id) VALUES('Forum','Tire suas dúvidas',2,'https://cdn.iconscout.com/icon/free/png-256/education-cap-2130827-1794972.png', 2,1);
+INSERT INTO tb_resource(title,description,position,img_Uri,type,offer_id) VALUES('Lives','Lives exclusivas',3,'https://cdn.iconscout.com/icon/free/png-256/education-cap-2130827-1794972.png', 0,1);
