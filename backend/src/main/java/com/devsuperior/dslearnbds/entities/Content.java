7b9ee852202a84ec88474b57ae6ab991821c930e
Henrique Bortolim <85265662+HBortolim@users.noreply.github.com>
2022-07-19 18:23:38 -0300

    Add files via upload

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Content.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Content.java
@@ -2,37 +2,37 @@ package com.devsuperior.dslearnbds.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "tb_content")
 public class Content extends Lesson {
+	private static final long serialVersionUID = 1L;
 
-    private String textContext;
-    private String videoUri;
+	private String textContent;
+	private String videoUri;
+	
+	public Content() {
+	}
 
-    public Content() {
-    }
+	public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+		super(id, title, position, section);
+		this.textContent = textContent;
+		this.videoUri = videoUri;
+	}
 
-    public Content(Long id, String title, Integer position, Section section, String textContext, String videoUri) {
-        super(id, title, position, section);
-        this.textContext = textContext;
-        this.videoUri = videoUri;
-    }
+	public String getTextContent() {
+		return textContent;
+	}
 
-    public String getTextContext() {
-        return textContext;
-    }
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
 
-    public void setTextContext(String textContext) {
-        this.textContext = textContext;
-    }
+	public String getVideoUri() {
+		return videoUri;
+	}
 
-    public String getVideoUri() {
-        return videoUri;
-    }
-
-    public void setVideoUri(String videoUri) {
-        this.videoUri = videoUri;
-    }
+	public void setVideoUri(String videoUri) {
+		this.videoUri = videoUri;
+	}
 }
--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -1,115 +1,134 @@
 package com.devsuperior.dslearnbds.entities;
 
-import com.devsuperior.dslearnbds.entities.enums.ResourceType;
-
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.devsuperior.dslearnbds.entities.enums.ResourceType;
+
 @Entity
 @Table(name = "tb_resource")
 public class Resource implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String title;
-    private String description;
-    private Integer position;
-    private String imgUri;
-    private ResourceType type;
-    //private String externalLink;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String description;
+	private Integer position;
+	private String imgUri;
+	private ResourceType type;
+	
+	@ManyToOne
+	@JoinColumn(name = "offer_id")
+	private Offer offer;
 
-    @ManyToOne
-    @JoinColumn(name = "offer_id")
-    private Offer offer;
+	@OneToMany(mappedBy = "resource")
+	private List<Section> sections = new ArrayList<>();
+	
+	public Resource() {
+	}
 
-    @OneToMany(mappedBy = "resource")
-    private List<Section> sections = new ArrayList<>();
+	public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,
+			Offer offer) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.position = position;
+		this.imgUri = imgUri;
+		this.type = type;
+		this.offer = offer;
+	}
 
-    public Resource() {
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type, Offer offer) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.position = position;
-        this.imgUri = imgUri;
-        this.type = type;
-        this.offer = offer;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public Integer getPosition() {
+		return position;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
 
-    public Integer getPosition() {
-        return position;
-    }
+	public String getImgUri() {
+		return imgUri;
+	}
 
-    public void setPosition(Integer position) {
-        this.position = position;
-    }
+	public void setImgUri(String imgUri) {
+		this.imgUri = imgUri;
+	}
 
-    public String getImgUri() {
-        return imgUri;
-    }
+	public ResourceType getType() {
+		return type;
+	}
 
-    public void setImgUri(String imgUri) {
-        this.imgUri = imgUri;
-    }
+	public void setType(ResourceType type) {
+		this.type = type;
+	}
 
-    public ResourceType getType() {
-        return type;
-    }
+	public Offer getOffer() {
+		return offer;
+	}
 
-    public void setType(ResourceType type) {
-        this.type = type;
-    }
+	public void setOffer(Offer offer) {
+		this.offer = offer;
+	}
 
-    public Offer getOffer() {
-        return offer;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    public void setOffer(Offer offer) {
-        this.offer = offer;
-    }
-
-    public List<Section> getSections() {
-        return sections;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Resource resource = (Resource) o;
-        return id.equals(resource.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resource other = (Resource) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
